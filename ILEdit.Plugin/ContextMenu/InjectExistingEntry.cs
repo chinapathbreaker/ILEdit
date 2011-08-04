993322eaea0ebb8e7c66b0532269c27452ff4d4f
Marco Cameriero <marco95ulisse@live.com>
2011-08-04 21:44:10 +0200

    Created project ILEdit.Plugin

---
+++ ILEdit.Plugin/ContextMenu/InjectExistingEntry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using ICSharpCode.TreeView;
+using ICSharpCode.ILSpy;
+
+namespace ILEdit.ContextMenu
+{
+    [ExportContextMenuEntry(Icon = "Images/InjectExisting.png", Header = "Inject existing ...", Category = "Injection", Order = 1)]
+    public class InjectExistingEntry : IContextMenuEntry
+    {
+        public bool IsVisible(SharpTreeNode[] selectedNodes)
+        {
+            return true;
+        }
+
+        public bool IsEnabled(SharpTreeNode[] selectedNodes)
+        {
+            return true;
+        }
+
+        public void Execute(SharpTreeNode[] selectedNodes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ILEdit.Plugin/ContextMenu/InjectNewEntry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using ICSharpCode.ILSpy;
+using ICSharpCode.TreeView;
+
+namespace ILEdit.ContextMenu
+{
+    [ExportContextMenuEntry(Icon = "Images/InjectNew.png", Header = "Inject new ...", Category = "Injection", Order = 0)]
+    public class InjectNewEntry : IContextMenuEntry
+    {
+        public bool IsVisible(SharpTreeNode[] selectedNodes)
+        {
+            return true;
+        }
+
+        public bool IsEnabled(SharpTreeNode[] selectedNodes)
+        {
+            return true;
+        }
+
+        public void Execute(SharpTreeNode[] selectedNodes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
